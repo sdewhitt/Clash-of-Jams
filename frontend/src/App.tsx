0e170773b3e773853393b01572397d3455e05d1a
bdravis <bdravis@purdue.edu>
2026-09-20 21:33:25 -0400

    09/20/26-bdravis-defined_user_profile_route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
 
 import { Home } from '@/pages/Home'
 import { Login } from '@/pages/Login'
+import { UserProfile } from '@/pages/UserProfile'
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<UserProfile />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
