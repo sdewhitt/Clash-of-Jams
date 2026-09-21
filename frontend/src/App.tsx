89986e15047748c72b2814a2972e647dfad6e1b6
bdravis <bdravis@purdue.edu>
2026-09-21 15:09:40 -0400

    09/21/26-bdravis-add_friends_navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
 import { Home } from '@/pages/Home'
 import { Login } from '@/pages/Login'
 import { UserProfile } from '@/pages/UserProfile'
+import { Friends } from '@/pages/Friends'
 import { Settings } from '@/pages/Settings'
 import { Tuner } from '@/pages/Tuner'
 import { ScenarioEditor } from '@/pages/ScenarioEditor'
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/profile" element={<UserProfile />} />
+        <Route path="/friends" element={<Friends />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/tuner" element={<Tuner />} />
         <Route path="/scenario_editor" element={<ScenarioEditor />} />
---
+++ frontend/src/pages/Friends.tsx
@@ -0,0 +1,8 @@
+export function Friends() {
+    
+    return (
+        <main className="min-h-dvh px-8 py-8">
+            <h1>Friends Page</h1>
+        </main>
+    )
+}
